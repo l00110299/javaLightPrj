310a02edbec28c20d1aeb4773756a5b20bfcd91f
Martin Farrell <l00110299@student.lyit.ie>
2018-11-19 20:18:28 +0000

    initial commit of project

---
+++ calc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class calc
+{
+	public static void main(String [] args)
+	{
+		int num1, num2;
+
+		Scanner keyboardIn = new Scanner(System.in);
+		System.out.println("enter num 1: ");
+		num1 = keyboardIn.nextInt();
+		System.out.println("Enter num 2 : ");
+		num2 = keyboardIn.nextInt();
+
+		System.out.println("inswer " +(num1 + num2));
+	}
+}
